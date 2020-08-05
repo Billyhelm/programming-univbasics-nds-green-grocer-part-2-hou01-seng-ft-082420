00508786952870dcc1807e29f4377d7d5d8c819a
Billy Helm <billyhelm88@gmail.com>
2020-08-05 20:11:09 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -4,7 +4,7 @@ def apply_coupons(cart, coupons)
   new_coupon = []
   coupons.map do |coupon|
     cart.map do |item_lookup|
-      if item_lookup[:item] === coupon[:item]
+      if item_lookup[:item] === coupon[:item] & item_lookup[:count] >= coupon[:num]
         item_lookup[:count] -= coupon[:num]
         item_price = coupon[:cost] / coupon[:num]
         cart.push({item: "#{coupon[:item]} W/COUPON", price: item_price, clearance: item_lookup[:clearance], count: coupon[:num]})
@@ -13,14 +13,6 @@ def apply_coupons(cart, coupons)
   end #coupons.map
   cart 
 end #method
-
-cart = [
-  {:item => "AVOCADO", :price => 3.00, :clearance => true, :count => 3},
-  {:item => "KALE",    :price => 3.00, :clearance => false, :count => 1}
-]
-coupons = [
-  {:item => "AVOCADO", :num => 2, :cost => 5.00}
-]
 
 def apply_clearance(cart)
   cart.map do |items|
