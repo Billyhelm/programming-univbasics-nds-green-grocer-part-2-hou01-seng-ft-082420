d6a256d6db3d44ca1aba3886d4c754eac6549e6f
Billy Helm <billyhelm88@gmail.com>
2020-08-05 19:56:29 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -25,7 +25,7 @@ coupons = [
 def apply_clearance(cart)
   cart.map do |items|
     if items[:clearance]
-      items[:price] = number_with_precision(items[:price]*0.8, precision: 2)
+      items[:price] = items[:price]*0.8, precision: 2
     end #if items
   end #cart.map 
   p cart 
