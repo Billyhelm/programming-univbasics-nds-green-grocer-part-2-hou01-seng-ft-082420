0d51777367439e3f91c1dbe6026fa8a61fe9e080
Billy Helm <billyhelm88@gmail.com>
2020-08-05 20:08:32 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -38,6 +38,7 @@ def checkout(cart, coupons)
   cart = apply_clearance(cart)
   total = 0
   cart.each{|item| total += (item[:price]*item[:count])}
+  total > 100 ? total *= .9 : total
   total
 end
 
